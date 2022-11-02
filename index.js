22c0c52941e153a26705990947ca979362b2fe41
Miskatur <Miskaturrahman34826@gmail.com>
2022-11-03 00:07:20 +0600

    basid node mongo db setup

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors")
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8r9nhhc.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send("Genius Car Server is Running")
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port :', port)
+})
